b72dd464f09dd4d5904469562e146edc64ca020f
John Xu <johnbxu@gmail.com>
2018-10-28 00:52:31 +0000

    created components initial

---
+++ src/MessageList.jsx
@@ -0,0 +1,12 @@
+import Message from './Message.jsx';
+
+import React, {Component} from 'react';
+
+class Messages extends Component {
+  render() {
+    return (
+      <Message />
+    );
+  }
+}
+export default Messages;
