631a43b55b605b8e188a133a9f91e5b8a4d1c912
John Xu <johnbxu@gmail.com>
2018-10-29 16:01:16 +0000

    added real time updating of messages across multiple clients

--- src/App.jsx
+++ src/App.jsx
@@ -9,57 +9,40 @@ class App extends Component {
     super(props),
     this.state = {
       currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-         {
-           id: '1',
-           username: 'Bob',
-           content: 'Has anyone seen my marbles?',
-         },
-         {
-           id: '2',
-           username: 'Anonymous',
-           content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.',
-         }
-       ]
+      messages: []
     }
     this.submitMessage = this.submitMessage.bind(this);
+    this.updateStateMessages = this.updateStateMessages.bind(this);
   }
   submitMessage (event) {
-    // const newMessage = {
-    //   username: event.
-    // }
     event.preventDefault()
     const newMessage = {
-      id: generateRandomString(8),
       username: event.target.username.value,
       content: event.target.content.value,
     }
 
     this.socket.send(JSON.stringify(newMessage));
-
-    const oldMessages = this.state.messages
-    const newMessages = [...oldMessages, newMessage]
-    this.setState({
-      currentUser: {name: event.target.username.value},
-      messages: newMessages,
-    })
     event.target.content.value = ''
   }
+
+  updateStateMessages(data) {
+    const receivedMessage = JSON.parse(data.data);
+    const oldMessages = this.state.messages;
+    const newMessages = [...oldMessages, receivedMessage]
+    this.setState({
+      currentUser: {name: receivedMessage.username},
+      messages: newMessages,
+    })
+    console.log(this.state)
+  }
+
   componentDidMount() {
+    const self = this;
     this.socket = new WebSocket('ws://0.0.0.0:3001');
     this.socket.onmessage = function(event) {
-      console.log(event.data)
+      self.updateStateMessages(event);
     }
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
   }
   render() {
     return (
